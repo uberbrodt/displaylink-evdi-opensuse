6725861ba5ef2985bbde67b7344dd8d58b37102f
Chris Brodt <chris@uberbrodt.net>
2016-11-27 16:45:47 -0600

    Support for driver version 1.2.65
    Broke out the udev patch into it's own shell script, otherwise just
    minor changes.

--- InstallDisplaylink.sh
+++ InstallDisplaylink.sh
@@ -3,14 +3,15 @@
 WORKDIR="build"
 
 # Download the Ubuntu Driver from the DisplayLink webiste
-wget -O ${WORKDIR}/DisplayLink_Ubuntu_1.1.62.zip http://www.displaylink.com/downloads/file?id=607
+wget -O ${WORKDIR}/DisplayLink_Ubuntu_1.2.65.zip http://www.displaylink.com/downloads/file?id=708
 
 # Unpack the driver
 cd ${WORKDIR}
-unzip DisplayLink_Ubuntu_1.1.62.zip
-chmod 755 displaylink-driver-1.1.62.run
-./displaylink-driver-1.1.62.run --noexec --keep
-cd displaylink-driver-1.1.62
+unzip DisplayLink_Ubuntu_1.2.65.zip
+chmod 755 displaylink-driver-1.2.65.run
+./displaylink-driver-1.2.65.run --noexec --keep
+cd displaylink-driver-1.2.65
 
 # Patch the installer, so it can installer everything on openSUSE Tumbleweed
 patch -p0 < ../../displaylink-installer.sh.opensuse.patch
+patch -p0 < ../../udev-installer.sh.opensuse.patch
--- README.md
+++ README.md
@@ -15,10 +15,10 @@ launch the script
 ./InstallDisplayLink.sh
 ```
 
-The script will download the Ubuntu driver 1.1.62, and apply the needed patch, so that it works for openSUSE Tumbleweed.
+The script will download the Ubuntu driver 1.2.65, and apply the needed patch, so that it works for openSUSE Tumbleweed.
 
-Just after enter in the displaylink-1.1.62 directory and launch the install script:
+Just after enter in the displaylink-1.2.65 directory and launch the install script:
 ```
-cd build/displaylink-1.1.62
+cd build/displaylink-1.2.65
 ./displaylink-installer.sh install
 ```
